b4df1523ccc6a0c2a9920a7194b3e28c94ae5ec2
zann10thousand <32677985+zann10thousand@users.noreply.github.com>
2017-10-10 09:49:15 -0500

    Add files via upload

---
+++ RNG.cpp
@@ -0,0 +1,25 @@
+// Random Numebr Generator
+#include "stdafx.h"
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	int Num, Range;
+	cout << "How many random numbers do you wish to output? ";
+	cin >> Num;
+	cout << "What is the highest possible roll? ";
+	cin >> Range;
+	srand(time(NULL));
+
+	for (int i = 0; i < Num; i += 1)
+	{
+		int rng = (rand()%Range) + 1;
+		cout << rng << endl;
+	}
+
+	system("PAUSE");
+	return 0;
+}
